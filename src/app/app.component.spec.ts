5e498764153790eadb075268c8484ab4c592efd9
Kenneth Phang <bunnyppl@gmail.com>
2022-03-22 02:10:57 +0800

    upd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bitcoin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Bitcoin Changer v2');
   });
 });
