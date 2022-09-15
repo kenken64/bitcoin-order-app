b8353f42abc7db94f9f23bb9bfb02cf0eca0878f
KENNETH PHANG <bunnyppl@hotmail.com>
2022-09-15 16:49:33 +0800

    upd

--- karma.conf.js
+++ karma.conf.js
@@ -28,8 +28,8 @@ module.exports = function (config) {
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
     	ChromeHeadlessCI: {
-    		base: 'ChromeHeadless',
-    		flags: ['--no-sandbox']
+	    	base: 'ChromeHeadless',
+    		flags: ['--no-sandbox', '--disable-gpu']
  	}
     },
     singleRun: false,
