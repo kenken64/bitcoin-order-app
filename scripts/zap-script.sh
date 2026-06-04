f0c073a9228ddf9ae8338754ce0f48a6d7f4e855
Codemelia <ameliaw2903@gmail.com>
2026-06-04 16:15:06 +0800

    lint, dast, sast 1.0

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://Codemelia.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
